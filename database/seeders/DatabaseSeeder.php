71c27ee7492968b5c3bc27889639b887aae6a039
Suryansh Singh <singhsuryansh2746@gmail.com>
2026-09-06 14:45:14 +0530

    admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,23 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::updateOrCreate(
+            [
+                'email' => 'admin@estele.com',
+            ],
+            [
+                'name' => 'Estelle Admin',
+                'password' => Hash::make('Admin@12345'),
+                'role' => 'admin',
+            ]
+        );
     }
 }
+
