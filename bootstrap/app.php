5508a75cc34c73d856de1cf7974f59defa3ab730
Suryansh Singh <singhsuryansh2746@gmail.com>
2026-09-06 21:31:56 +0530

    bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,10 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
         );
     })->create();
+
+    
