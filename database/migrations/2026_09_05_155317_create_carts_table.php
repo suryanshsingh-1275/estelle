0ab2b4ab2bda214854895f95f7df6b431ed606ca
Suryansh Singh <singhsuryansh2746@gmail.com>
2026-09-06 14:49:39 +0530

    cart migration

--- database/migrations/2026_09_05_155317_create_carts_table.php
+++ database/migrations/2026_09_05_155317_create_carts_table.php
@@ -6,22 +6,30 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('user_id')
+                  ->constrained('users')
+                  ->cascadeOnDelete();
+
+            $table->string('product_name');
+
+            $table->string('product_image')->nullable();
+
+            $table->decimal('price', 10, 2);
+
+            $table->unsignedInteger('quantity')->default(1);
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('carts');
     }
 };
+
