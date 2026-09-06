24640e4196967b1c02b299f4b6f63613d62e0e75
Suryansh Singh <singhsuryansh2746@gmail.com>
2026-09-06 14:51:58 +0530

    cart model

---
+++ app/Models/Cart.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Cart extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'product_name',
+        'product_image',
+        'price',
+        'quantity',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'price' => 'decimal:2',
+            'quantity' => 'integer',
+        ];
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
+
