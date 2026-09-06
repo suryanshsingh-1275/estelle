69901a4b739bcb44d6b1f4402fe7d499cbcf7921
Suryansh Singh <singhsuryansh2746@gmail.com>
2026-09-06 15:03:01 +0530

    admin route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,63 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CustomerController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+/*
+|--------------------------------------------------------------------------
+| Customer
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/', [CustomerController::class, 'home'])
+    ->name('home');
+
+Route::get('/login', [CustomerController::class, 'loginPage'])
+    ->name('login');
+
+Route::get('/signup', [CustomerController::class, 'signupPage'])
+    ->name('signup');
+
+
+/*
+|--------------------------------------------------------------------------
+| Customer POST APIs
+|--------------------------------------------------------------------------
+*/
+
+Route::post('/api/signup', [AuthController::class, 'signup'])
+    ->name('api.signup');
+
+Route::post('/api/login', [AuthController::class, 'login'])
+    ->name('api.login');
+
+Route::post('/api/logout', [AuthController::class, 'logout'])
+    ->name('api.logout');
+
+Route::post('/api/cart/add', [CartController::class, 'add'])
+    ->name('api.cart.add');
+
+Route::post('/api/cart/remove/{cart}', [CartController::class, 'remove'])
+    ->name('api.cart.remove');
+
+
+/*
+|--------------------------------------------------------------------------
+| Cart
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/cart', [CartController::class, 'index'])
+    ->name('cart');
+
+
+/*
+Admin
+*/
+
+Route::get('/admin', [AdminController::class, 'dashboard'])
+    ->name('admin.dashboard');
